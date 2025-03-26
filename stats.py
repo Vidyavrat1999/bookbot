af4bf00c769ab52c48cc383f8d12e5fc8c859608
Vidyavrat Taak <reshamsing95@gmail.com>
2025-03-26 14:01:24 +0530

    bookbot start

---
+++ stats.py
@@ -0,0 +1,26 @@
+def word_count(text):
+    words = len(text.split())
+    return words
+
+
+def each_charcter_appearence(text):
+    char_count = {}
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
